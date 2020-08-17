aac7560733766ba34666c428131836cfa6faa1c9
Thomaz Peres <thomaz.peres21@hotmail.com>
2020-08-17 12:09:03 -0300

    Finish application

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,52 +1,95 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Biblioteca.Data;
 using Biblioteca.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 [Route("obras")]
 public class ProductController : ControllerBase
 {
     [HttpGet]
     [Route("")]
-    public async Task<ActionResult<List<Product>>> Get()
+    public async Task<ActionResult<List<Product>>> Get([FromServices]DataContext context)
     {
-        return new List<Product>();
+        var produtos = await context.Products.AsNoTracking().ToListAsync();
+        return Ok(produtos);
     }
 
     [HttpGet]
     [Route("{genero}")]
-    public async Task<ActionResult<Product>> GetByGenero(string genero)
+    public async Task<ActionResult<Product>> GetByGenero(
+        string genero,
+        [FromServices]DataContext context)
     {
-        return new Product();
+        var products = await context.Products.AsNoTracking().FirstOrDefaultAsync(x => x.Genero == genero);
+        return Ok(products); 
     }
 
     [HttpPost]
     [Route("")]
-    public async Task<ActionResult<List<Product>>> Post([FromBody]Product model)
+    public async Task<ActionResult<List<Product>>> Post(
+        [FromBody]Product model,
+        [FromServices]DataContext context)
     {
         if (!ModelState.IsValid)
             return BadRequest(ModelState);
         
-        return Ok(model);
+        try
+        {
+            context.Products.Add(model);
+            await context.SaveChangesAsync();
+            return Ok(model);
+        }
+        catch (Exception)
+        {
+            return BadRequest (new { message = "Não foi possivel criar o livro" });
+        }
     }
 
     [HttpPut]
     [Route("{id:int}")]
-    public async Task<ActionResult<List<Product>>> Put(int id, [FromBody]Product model)
+    public async Task<ActionResult<List<Product>>> Put(int id, 
+        [FromBody]Product model,
+        [FromServices]DataContext context)
     {
-        if(id != model.Id)
-            return NotFound(new { message = "Categoria nçao encontrada" });
+        if (id != model.Id)
+            return NotFound(new { message = "Livro não encontrada" });
 
-        if (!ModelState.IsValid)
+        if(!ModelState.IsValid)
             return BadRequest(ModelState);
 
-        return Ok();
+        try{
+            context.Entry<Product>(model).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return Ok(model);
+        }
+        catch (Exception)
+        {
+            return BadRequest(new { message = "Não foi possivel atualizar o livro" });
+        }
     }
 
     [HttpDelete]
     [Route("{id:int}")]
-    public async Task<ActionResult<List<Product>>> Delete()
+    public async Task<ActionResult<List<Product>>> Delete(
+        int id,
+        [FromServices]DataContext context)
     {
-        return Ok("Livro deletado com sucesso");
+        var product = await context.Products.FirstOrDefaultAsync(x => x.Id == id);
+            if (product == null)
+                return NotFound(new { message = "Livro não encontrado" });
+                
+        try
+        {
+            context.Products.Remove(product);
+            await context.SaveChangesAsync();
+            return Ok(new { message = "Livro removido com sucesso" });
+        }
+        catch (Exception)
+        {
+            return BadRequest(new { message = "Não foi possivel remover o livro" });
+        }
     }
 }
